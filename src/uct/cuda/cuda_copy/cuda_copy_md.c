b352f4dff7088eee79e53fbe551c57a67cd1574d
Devendar Bureddy <devendar@mellanox.com>
2017-08-08 15:50:10 -0700

    UCT: cuda_copy uct reg/dereg function

--- src/uct/cuda/cuda_copy/cuda_copy_md.c
+++ src/uct/cuda/cuda_copy/cuda_copy_md.c
@@ -12,6 +12,8 @@
 #include <ucs/sys/sys.h>
 #include <ucs/debug/memtrack.h>
 #include <ucs/type/class.h>
+#include <cuda_runtime.h>
+#include <cuda.h>
 
 
 static ucs_status_t uct_cuda_copy_md_query(uct_md_h md, uct_md_attr_t *md_attr)
@@ -47,26 +49,37 @@ static ucs_status_t uct_cuda_copy_rkey_release(uct_md_component_t *mdc, uct_rkey
     return UCS_OK;
 }
 
-static ucs_status_t uct_cuda_mem_reg(uct_md_h md, void *address, size_t length,
+static ucs_status_t uct_cuda_copy_mem_reg(uct_md_h md, void *address, size_t length,
                                      unsigned flags, uct_mem_h *memh_p)
 {
-    ucs_status_t rc;
-    uct_mem_h * mem_hndl = NULL;
-    mem_hndl = ucs_malloc(sizeof(void *), "cuda handle for test passing");
-    if (NULL == mem_hndl) {
-      ucs_error("Failed to allocate memory for gni_mem_handle_t");
-      rc = UCS_ERR_NO_MEMORY;
-      goto mem_err;
+    cudaError_t cuerr = cudaSuccess;
+
+    if(address == NULL) {
+        *memh_p = address;
+        return UCS_OK;
     }
-    *memh_p = mem_hndl;
+
+    cuerr = cudaHostRegister(address, length, cudaHostRegisterPortable);
+    if (cuerr != cudaSuccess) {
+        return UCS_ERR_IO_ERROR;
+    }
+
+    *memh_p = address;
     return UCS_OK;
- mem_err:
-    return rc;
 }
 
-static ucs_status_t uct_cuda_mem_dereg(uct_md_h md, uct_mem_h memh)
+static ucs_status_t uct_cuda_copy_mem_dereg(uct_md_h md, uct_mem_h memh)
 {
-    ucs_free(memh);
+    void *address = (void *)memh;
+    cudaError_t cuerr = cudaSuccess;
+    if (address == NULL) {
+        return UCS_OK;
+    }
+    cuerr = cudaHostUnregister(address);
+    if (cuerr != cudaSuccess) {
+        return UCS_ERR_IO_ERROR;
+    }
+
     return UCS_OK;
 }
 
